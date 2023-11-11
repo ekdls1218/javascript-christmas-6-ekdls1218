e88857680cc6edb2e4c089a408f6ea67932364c6
김다인 <dbslzhs1218@naver.com>
2023-11-12 07:00:27 +0900

    feat(christmas): 특별 할인 기능 구현
    - 일요일에만 할인이 해당되기 때문에 요일 찾기 메서드 구현
    - date도 private로 선언해주기

--- src/Event.js
+++ src/Event.js
@@ -1,11 +1,16 @@
 import CustomError from './CustomError.js';
 
 class Event {
+  #date;
+
   #discount;
+
+  #dayOfWeek;
 
   constructor(date) {
     this.checkDate(date);
-    this.date = date;
+    this.#date = date;
+    this.getDayOfWeek(date);
   }
 
   checkDate(date) {
@@ -17,8 +22,8 @@ class Event {
   checkDdayEvent() {
     this.resetDiscount();
 
-    if (this.date >= 1 && this.date <= 25) {
-      this.#discount = 1000 + (this.date - 1) * 100;
+    if (this.#date >= 1 && this.#date <= 25) {
+      this.#discount = 1000 + (this.#date - 1) * 100;
     }
 
     return this.#discount;
@@ -29,6 +34,22 @@ class Event {
     this.#discount = count * 2023;
 
     return this.#discount;
+  }
+
+  checkSpecialEvent() {
+    this.resetDiscount();
+
+    if (this.#dayOfWeek === 0 || this.#date === 25) {
+      this.#discount = 1000;
+    }
+
+    return this.#discount;
+  }
+
+  getDayOfWeek(date) {
+    this.#dayOfWeek = new Date(2023, 11, date).getDay();
+
+    return this.#dayOfWeek;
   }
 
   resetDiscount() {
