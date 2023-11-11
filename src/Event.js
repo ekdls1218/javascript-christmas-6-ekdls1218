2c275babc176632879c3657739331006f794c894
김다인 <dbslzhs1218@naver.com>
2023-11-12 05:57:35 +0900

    feat(christmas): 이벤트 날짜 체크 및 크리스마스 디데이 할인 기능 구현

--- src/Event.js
+++ src/Event.js
@@ -1,18 +1,24 @@
+import CustomError from './CustomError.js';
+
 class Event {
-  #date;
-
-  #discountAmount;
-
   constructor(date) {
-    this.#date = date;
+    this.checkDate(date);
+    this.date = date;
   }
 
   checkDate(date) {
-    return date;
+    if (date < 1 || date > 31) {
+      throw new CustomError('날짜를 다시 입력해주세요.');
+    }
   }
 
-  discount() {
-    return null;
+  checkDdayEvent() {
+    let discount = 0;
+    if (this.date >= 1 && this.date <= 25) {
+      discount = 1000 + (this.date - 1) * 100;
+    }
+
+    return discount;
   }
 }
 export default Event;
