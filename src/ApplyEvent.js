75ec38cb60473d19f8615d0f51c72a6fd8abc47a
김다인 <dbslzhs1218@naver.com>
2023-11-13 03:08:11 +0900

    feat(christmas): 할인 전 총 주문 금액 계산 기능 구현
    - 아직 입력 기능을 구현하지 않았기 때문에 임시로 메뉴 값을 넣어줌

--- src/ApplyEvent.js
+++ src/ApplyEvent.js
@@ -3,12 +3,34 @@ class ApplyEvent {
 
   #inputMenu;
 
-  #inputMenuCount;
+  #discountEvent;
 
-  constructor(inputDate, inputMenu, inputMenuCount) {
+  #orderAmount;
+
+  // 임시 구현
+  menu = [
+    { name: '티본스테이크', price: 55000, type: 'main', count: 1 },
+    { name: '바비큐립', price: 54000, type: 'main', count: 1 },
+    { name: '초코케이크', price: 15000, type: 'dessert', count: 2 },
+    { name: '제로콜라', price: 3000, type: 'drink', count: 1 },
+  ];
+
+  constructor(inputDate, inputMenu) {
     this.#inputDate = inputDate;
     this.#inputMenu = inputMenu;
-    this.#inputMenuCount = inputMenuCount;
+    this.#orderAmount = this.calculateOrderAmount();
+    this.#discountEvent = new Event(inputDate);
+  }
+
+  calculateOrderAmount() {
+    let sumPrice;
+    this.#orderAmount = this.menu.reduce((accumulator, order) => {
+      sumPrice = accumulator + order.price * order.count;
+
+      return sumPrice;
+    }, 0);
+
+    return this.#orderAmount;
   }
 }
 export default ApplyEvent;
