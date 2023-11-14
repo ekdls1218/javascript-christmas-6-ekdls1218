89430a6b5c8612cf9d7730026493e9ceed381cbc
김다인 <dbslzhs1218@naver.com>
2023-11-14 20:16:58 +0900

    style(christmas): 혜택 메세지 내역 상수화

--- src/constants/prompt.js
+++ src/constants/prompt.js
@@ -8,11 +8,21 @@ const PROMPT = Object.freeze({
   PREVIEW: '에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n',
   ORDER_MENU: '<주문 메뉴>',
   TOTAL_AMOUNT: '\n<할인 전 총주문 금액>',
-  PRESENT: '\n<증정 메뉴>',
+  PRESENT_MENU: '\n<증정 메뉴>',
   BENEFIT: '\n<혜택 내역>',
   TOTAL_BENEFIT: '\n<총혜택 금액>',
   PAYMENT: '\n<할인 후 예상 결제 금액>',
   BADGE: `\n<${STRINGS.MONTH} 이벤트 배지>`,
+
+  PRESENT: '샴페인 1개',
+
+  EVENT: [
+    '크리스마스 디데이 할인:',
+    '평일 할인:',
+    '주말 할인:',
+    '특별 할인:',
+    '증정 이벤트:',
+  ],
 
   NOTHING: '없음',
 
