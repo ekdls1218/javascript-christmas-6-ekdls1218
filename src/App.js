d201783791ea529d8f74d37ffa5faefcf40d8027
김다인 <dbslzhs1218@naver.com>
2023-11-14 23:57:55 +0900

    feat:(christmas): 날짜, 주문서 얻기 및 ApplyEvent 클래스 생성 기능 구현
    - 입력 받은 주문 메뉴와 개수는 정제하여 메뉴 이름, 메뉴 가격, 메뉴 종류,
      주문 개수를 가진 객체 배열로 만들기
    - 입력받은 날짜와 주문서는 ApplyEvent 클래스 생성 시 인자로 넘겨줌

--- src/App.js
+++ src/App.js
@@ -1,5 +1,67 @@
+import ApplyEvent from './ApplyEvent.js';
+import InputView from './InputView.js';
+import OutputView from './OutputView.js';
+import menu from './constants/menu.js';
+import PROMPT from './constants/prompt.js';
+
 class App {
-  async run() {}
+  #visitDate;
+
+  #orderSheet;
+
+  #applyEvent;
+
+  constructor() {
+    this.#visitDate = '';
+    this.#orderSheet = [];
+  }
+
+  async run() {
+    OutputView.print(PROMPT.INTRODUCE);
+    await this.inputDate();
+    await this.inputMenu();
+    this.#applyEvent = new ApplyEvent(this.#visitDate, this.#orderSheet);
+  }
+
+  async inputDate() {
+    this.#visitDate = await InputView.readDate();
+
+    // 에러처리
+  }
+
+  async inputMenu() {
+    const inputOrderMenu = await InputView.readOrder();
+
+    // 에러 처리
+
+    this.#orderSheet = this.orderMenu(inputOrderMenu);
+  }
+
+  orderMenu(inputOrderMenu) {
+    const commaSeparate = inputOrderMenu.split(',');
+
+    const order = [];
+    commaSeparate.forEach((eachMenu) => {
+      order.push(...this.refineOrder(eachMenu));
+    });
+
+    return order;
+  }
+
+  refineOrder(eachMenu) {
+    const hyphenSeparate = eachMenu.split('-');
+
+    const orderFiltering = menu.filter(
+      (vlaue) => vlaue.name === hyphenSeparate[0],
+    );
+
+    const insertCount = orderFiltering.map((value) => ({
+      ...value,
+      count: Number(hyphenSeparate[1]),
+    }));
+
+    return insertCount;
+  }
 }
 
 export default App;
