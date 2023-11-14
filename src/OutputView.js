f596d4051184033844c5b438115a05fd82de2d70
김다인 <dbslzhs1218@naver.com>
2023-11-14 23:43:46 +0900

    feat(christmas): 출력 기능을 가진 OutputView 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,26 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+import { STRINGS } from './constants/strings.js';
+import PROMPT from './constants/prompt.js';
+
+const OutputView = {
+  print(message) {
+    Console.print(message);
+  },
+
+  previewPrint(inputDate) {
+    Console.print(`${STRINGS.MONTH} ${inputDate}일${PROMPT.PREVIEW}`);
+  },
+
+  printOrderMenu(name, count) {
+    Console.print(`${name} ${count}개`);
+  },
+
+  printPrice(price) {
+    Console.print(`${price}원`);
+  },
+
+  printBenefit(name, discount) {
+    Console.print(`${name} ${discount}원`);
+  },
+};
+export default OutputView;
