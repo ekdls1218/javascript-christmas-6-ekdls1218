a251dd899a4c7d304dd3fa94290a49af0343eb2c
김다인 <dbslzhs1218@naver.com>
2023-11-15 04:17:15 +0900

    feat(christmas): 날짜 커스텀 에러 메서드 구현

--- src/CustomError.js
+++ src/CustomError.js
@@ -1,7 +1,13 @@
+import ERROR from './constants/error.js';
+
 class CustomError extends Error {
   constructor(message, name) {
-    super(`${message}`);
+    super(`${ERROR.ERROR} ${message}`);
     this.name = name;
+  }
+
+  static date(message) {
+    return new CustomError(message, ERROR.name.DATE);
   }
 }
 export default CustomError;
