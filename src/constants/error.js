6b07baa3552d97fde47e423cbaa5cd04f0df010b
김다인 <dbslzhs1218@naver.com>
2023-11-15 22:57:19 +0900

    style(christmas): 에러 상수 메세지 수정

--- src/constants/error.js
+++ src/constants/error.js
@@ -7,7 +7,7 @@ const ERROR = Object.freeze({
     EVENT: 'evnet input error',
   },
 
-  INVALID_DATE: '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
-  INVALID_ORDER: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
+  INVALID_DATE: '유효하지 않은 날짜입니다. 다시 입력해 주세요.\n',
+  INVALID_ORDER: '유효하지 않은 주문입니다. 다시 입력해 주세요.\n',
 });
 export default ERROR;
